ba0b86820b2f1c2ebe84632225ce2bc7717eb85a
harimasa <144851980+HeMatik@users.noreply.github.com>
2024-03-28 22:34:14 +0300

    Update
    [+] Added swapEveryHit, every hit/use changes hands.
    
    [+] Reduced the swap cooldown

--- src/main/java/me/matt/HandSwitcher.java
+++ src/main/java/me/matt/HandSwitcher.java
@@ -6,12 +6,16 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class HandSwitcher implements ModInitializer {
 	private static KeyBinding swapHandKeyBinding;
+	private static KeyBinding swapEveryHit;
 	private static long lastPressTime = 0;
-	private static final long COOLDOWN_MILLIS = 500;
+	private static final long COOLDOWN_MILLIS = 100;
+	private static boolean swapEveryHitEnabled = false;
+	private static boolean handSwappedThisSwing = false;
 
 	@Override
 	public void onInitialize() {
@@ -23,12 +27,38 @@ public class HandSwitcher implements ModInitializer {
 		));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> onTick());
+		swapEveryHit = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"SwapEveryHit",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_K,
+				"HandSwitcher"
+		));
 	}
 
 	public void onTick() {
+		MinecraftClient client = MinecraftClient.getInstance();
 		if (swapHandKeyBinding.isPressed() && System.currentTimeMillis() - lastPressTime > COOLDOWN_MILLIS) {
 			swapHand();
 			lastPressTime = System.currentTimeMillis();
+		}
+
+		if (swapEveryHit.isPressed() && System.currentTimeMillis() - lastPressTime > COOLDOWN_MILLIS) {
+			swapEveryHitEnabled = !swapEveryHitEnabled;
+			if (swapEveryHitEnabled) {
+				client.player.sendMessage(Text.of("§fSwapEveryHit §aenabled"));
+			} else {
+				client.player.sendMessage(Text.of("§fSwapEveryHit §cdisabled"));
+			}
+			lastPressTime = System.currentTimeMillis();
+		}
+
+		if (swapEveryHitEnabled) {
+			if (client.player != null && client.player.handSwingProgress > 0 && !handSwappedThisSwing) {
+				swapHand();
+				handSwappedThisSwing = true;
+			} else if (client.player != null && client.player.handSwingProgress == 0) {
+				handSwappedThisSwing = false;
+			}
 		}
 	}
 
